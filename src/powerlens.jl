ec25573a81843d2185bfd05d767bb2b0dcf80df7
marius <mmillea@ucdavis.edu>
2017-05-12 17:55:04 +0200

    allow PowerLens{order} as type parameter

--- src/powerlens.jl
+++ src/powerlens.jl
@@ -12,34 +12,33 @@
 
 export PowerLens
 
-@∷ struct PowerLens{F<:Field{∷,<:S0}} <: LenseOp
-    order::Int
+@∷ struct PowerLens{N,F<:Field{∷,<:S0}} <: LenseOp
     ∂xϕⁱ::Dict{Int,Union{Int,F}}
     ∂yϕⁱ::Dict{Int,Union{Int,F}}
 end
 
-function PowerLens(ϕ; order=4)
+function PowerLens{N}(ϕ) where {N}
     ∂xϕ, ∂yϕ = Ł(∂x*ϕ), Ł(∂y*ϕ)
-    PowerLens{typeof(∂xϕ)}(order,(Dict(i=>(i==0?1:∂ϕ.^i) for i=0:order) for ∂ϕ=(∂xϕ,∂yϕ))...)
+    PowerLens{N,typeof(∂xϕ)}((Dict(i=>(i==0?1:∂ϕ.^i) for i=0:N) for ∂ϕ=(∂xϕ,∂yϕ))...)
 end
 
 """ Create from an existing PowerLens operator one that lenses by -ϕ instead. """
-antilensing(L::PowerLens{F}) where {F} = PowerLens{F}(L.order, (Dict(i=>v*(-1)^i for (i,v)=∂) for ∂=(L.∂xϕⁱ,L.∂xϕⁱ))...)
+antilensing(L::PowerLens{N,F}) where {N,F} = PowerLens{N,F}(N, (Dict(i=>v*(-1)^i for (i,v)=∂) for ∂=(L.∂xϕⁱ,L.∂xϕⁱ))...)
 
 
-function *(L::PowerLens, f::Field)
+function *(L::PowerLens{N}, f::Field) where {N}
     f̂ = Ð(f)
     f̃ = 1Ł(f)
-    for n in 1:L.order, (a,b) in zip(0:n,n:-1:0)
+    for n in 1:N, (a,b) in zip(0:n,n:-1:0)
         @. f̃ += L.∂xϕⁱ[a] * L.∂yϕⁱ[b] * $Ł(∂x^a * ∂y^b * f̂) / factorial(a) / factorial(b)
     end
     f̃
 end
 
-function *(f::Field, L::PowerLens)
+function *(f::Field, L::PowerLens{N}) where {N}
     Łf = Ł(f)
     r = 1Ð(f)
-    for n in 1:L.order, (a,b) in zip(0:n,n:-1:0)
+    for n in 1:N, (a,b) in zip(0:n,n:-1:0)
         @. r += (-1)^n * ∂x^a * ∂y^b * $Ð(@. L.∂xϕⁱ[a] * L.∂yϕⁱ[b] * Łf) / factorial(a) / factorial(b)
     end
     r
@@ -58,12 +57,12 @@ end
 ## Jacobian terms
 
 """ δf̃(f,ϕ)/δϕ * v """
-function δf̃_δϕ(L::PowerLens, f::F, v::Field) where {F<:Field}
+function δf̃_δϕ(L::PowerLens{N}, f::F, v::Field) where {N,F<:Field}
     Ðf = Ð(f)
     r = Ł(zero(F))
     ∂ⁿf = Ł(zero(F))
     ∂xv, ∂yv = Ł(∇*v)
-    for n in 1:L.order, (a,b) in zip(0:n,n:-1:0)
+    for n in 1:N, (a,b) in zip(0:n,n:-1:0)
         @. ∂ⁿf = $Ł(@. ∂x^a * ∂y^b * Ðf) / factorial(a) / factorial(b)
         @. r += (  ((a==0) ? 0 : a * L.∂xϕⁱ[a-1] * L.∂yϕⁱ[b] * ∂xv * ∂ⁿf)
                  + ((b==0) ? 0 : b * L.∂xϕⁱ[a] * L.∂yϕⁱ[b-1] * ∂yv * ∂ⁿf))
@@ -78,22 +77,22 @@ end
 ## Jacobian transpose terms
 
 """ (δf̃(f,ϕ)/δf)ᴴ * v """
-function δf̃_δfᴴ(L::PowerLens, v::Field)
+function δf̃_δfᴴ(L::PowerLens{N}, v::Field) where {N}
     Łv = Ł(v)
     r = 1Ð(v)
-    for n in 1:L.order, (a,b) in zip(0:n,n:-1:0)
+    for n in 1:N, (a,b) in zip(0:n,n:-1:0)
         @. r += (-1)^n * ∂x^a * ∂y^b * $Ð(@. L.∂xϕⁱ[a] * L.∂yϕⁱ[b] * Łv) / factorial(a) / factorial(b)
     end
     r
 end
 
 """ (δf̃(f,ϕ)/δϕ)ᴴ * v """
-function δf̃_δϕᴴ(L::PowerLens{F}, f::Field, v::Field) where {F}
+function δf̃_δϕᴴ(L::PowerLens{N,F}, f::Field, v::Field) where {N,F}
     Łv = Ł(v)
     Ðf = Ð(f)
     r = Ð(zero(F))
     ∂ⁿfᴴ_v = Ł(zero(F))
-    for n in 1:L.order, (a,b) in zip(0:n,n:-1:0)
+    for n in 1:N, (a,b) in zip(0:n,n:-1:0)
         @. ∂ⁿfᴴ_v = $(Ł(@. ∂x^a * ∂y^b * Ðf)' * Łv) / factorial(a) / factorial(b)
         @. r += -(  ((a==0) ? 0 : (∂x * $Ð(@. a * L.∂xϕⁱ[a-1] * L.∂yϕⁱ[b] * ∂ⁿfᴴ_v)))
                   + ((b==0) ? 0 : (∂y * $Ð(@. b * L.∂xϕⁱ[a] * L.∂yϕⁱ[b-1] * ∂ⁿfᴴ_v))))
@@ -104,13 +103,13 @@ end
 ## Lensing second derivatives
 
 """ Compute wᴴ * δ²f̃(f,ϕ)/δϕ² * v """
-function δ²f̃_δϕ²(L::PowerLens{F}, f::Field, w::Field, v::Field) where {F}
+function δ²f̃_δϕ²(L::PowerLens{N,F}, f::Field, w::Field, v::Field) where {N,F}
     Łw = Ł(w)
     Ðf = Ð(f)
     ∂xv, ∂yv = Ł(∇*v)
     r = Ð(zero(F))
     ∂ⁿfᴴ_w = Ł(zero(F))
-    for n in 1:L.order, (a,b) in zip(0:n,n:-1:0)
+    for n in 1:N, (a,b) in zip(0:n,n:-1:0)
         @. ∂ⁿfᴴ_w = $(Ł(@. ∂x^a * ∂y^b * Ðf)' * Łw) / factorial(a) / factorial(b)
         @. r += -(  ((a<2)        ? 0 : (∂x * $Ð(@. ∂xv * a * (a-1) * L.∂xϕⁱ[a-2] * L.∂yϕⁱ[b]   * ∂ⁿfᴴ_w)))
                   + ((a<1 || b<1) ? 0 : (∂x * $Ð(@. ∂yv * a * b     * L.∂xϕⁱ[a-1] * L.∂yϕⁱ[b-1] * ∂ⁿfᴴ_w)))
