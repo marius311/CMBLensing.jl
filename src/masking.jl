8bc6e53eb3d3abd2386e7518cb2e11db52764bb5
marius <mmillea@ucdavis.edu>
2017-05-24 03:35:28 +0200

    fix boundary padding last pixel

--- src/masking.jl
+++ src/masking.jl
@@ -53,7 +53,7 @@ end
 function boundarymask(nside,Θpix,paddeg=3)
     pad = round(Int,paddeg*60/Θpix)
     m = fill(true,nside,nside)
-    m[1:pad,:] = m[end-pad:end,:] = m[:,1:pad] = m[:,end-pad:end] = false
+    m[1:pad,:] = m[end-pad+1:end,:] = m[:,1:pad] = m[:,end-pad+1:end] = false
     m
 end
 
