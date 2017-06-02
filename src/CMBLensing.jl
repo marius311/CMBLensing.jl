d2e129c7f6f6d922b25363e4e36545252546d161
marius <mmillea@ucdavis.edu>
2017-06-02 15:57:05 +0200

    tweak getindex for TEB tuples

--- src/CMBLensing.jl
+++ src/CMBLensing.jl
@@ -175,7 +175,7 @@ function getindex(f::F,x::Symbol) where {P,S,B,F<:Field{P,S,B}}
         error("Ambiguous field. Multiple subtypes of $F have a field $x: $l")
     end
 end 
-getindex(f::Field2Tuple{<:Field{<:Any,<:S0},<:Field{<:Any,<:S0}},s::Symbol) = startswith(string(s),"T") ? f.f1[s] : f.f2[s]
+getindex(f::Field2Tuple{<:Field{<:Any,<:S0},<:Field{<:Any,<:S2}},s::Symbol) = startswith(string(s),"T") ? f.f1[s] : f.f2[s]
 
 # submodules
 include("minimize.jl")
