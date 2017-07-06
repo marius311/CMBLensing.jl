ff281c8b24271318e8349e08b9bece0230e769f3
marius <mmillea@ucdavis.edu>
2017-07-06 17:28:00 +0200

    run2 tweaks for masking, sampling r

--- scripts/run2.jl
+++ scripts/run2.jl
@@ -1,6 +1,6 @@
 using JLD
 using CMBLensing
-using CMBLensing: @dictpack, jrk4, δlnΠᶠ_δfϕ
+using CMBLensing: @dictpack, jrk4, δlnΠᶠ_δfϕ, cache, LP, HP
 using Optim
 using PyCall
 using Base.Iterators: repeated
@@ -26,14 +26,17 @@ function run2(;
     Θpix = 3,
     nside = 64,
     T = Float32,
-    r = 0.05,
     L = LenseFlow{jrk4{7}},
     outfile = nothing,
     seed = nothing,
     mask = nothing,
-    Cℓf = nothing,
+    r = 0.05,
+    r_data = 0.05,
+    Cℓf = class(lmax=8000,r=r),
+    Cℓf_data = (r==r_data ? Cℓf : class(lmax=8000,r=r_data)),
     use = :TEB,
     ℓmax_data = 3000,
+    ℓmax_masking_hack = 10000,
     μKarcminT = 1,
     beamFWHM = 1.5,
     ℓknee = 100,
@@ -43,28 +46,18 @@ function run2(;
     cgtol = 1e-4)
     
     # Cℓs
-    Cℓf==nothing && (Cℓf = class(lmax=8000,r=r))
     Cℓf̃ = Dict(k=>Cℓf[Symbol("ln_$k")] for (k,v) in Cℓf if Symbol("ln_$k") in keys(Cℓf))
     Cℓn = noisecls(μKarcminT, beamFWHM=beamFWHM, ℓknee=ℓknee)
     
+    # types which depend on whether T/E/B
+    SS,ks = Dict(:TEB=>((S0,S2),(:tt,:ee,:bb,:te)), :EB=>((S2,),(:ee,:bb)), :T=>((S0,),(:tt,)))[use]
+    F,F̂,nF = Dict(:TEB=>(FlatIQUMap,FlatTEBFourier,3), :EB=>(FlatS2QUMap,FlatS2EBFourier,2), :T=>(FlatS0Map,FlatS0Fourier,1))[use]
+    
     ## covariances
     P = Flat{Θpix,nside}
-    SS,ks = Dict(:TEB=>((S0,S2),(:tt,:ee,:bb,:te)), :EB=>((S2,),(:ee,:bb)), :T=>((S0,),(:tt,)))[use]
-    Cϕ = Cℓ_to_cov(T,P,S0,    Cℓf[:ℓ], Cℓf[:ϕϕ])
-    Cf = Cℓ_to_cov(T,P,SS..., Cℓf[:ℓ], (Cℓf[k] for k=ks)...)
-    Cf̃ = Cℓ_to_cov(T,P,SS..., Cℓf[:ℓ], (Cℓf̃[k] for k=ks)...)
-    Cn = Cℓ_to_cov(T,P,SS..., Cℓn[:ℓ], (Cℓn[k] for k=ks)...)
+    Cϕ = Cℓ_to_cov(T,P,S0, Cℓf[:ℓ], Cℓf[:ϕϕ])
+    Cf,Cf̃,Cn,Cf_data = (Cℓ_to_cov(T,P,SS..., Cℓf[:ℓ], (Cℓx[k] for k=ks)...) for Cℓx in (Cℓf,Cℓf̃,Cℓn,Cℓf_data))
     
-    # data mask
-    F,F̂,nF = Dict(:TEB=>(FlatIQUMap,FlatTEBFourier,3), :EB=>(FlatS2QUMap,FlatS2EBFourier,2), :T=>(FlatS0Map,FlatS0Fourier,1))[use]
-    Mdf = FullDiagOp(F̂{T,P}(repeated(Cℓ_2D(P,1:ℓmax_data,ones(ℓmax_data)),nF)...))
-    if mask!=nothing
-        Mdr = FullDiagOp(F{T,P}(repeated(T.(sptlike_mask(nside,Θpix; (mask==true?():mask)...)),nF)...))
-    else
-        Mdr = 1
-    end
-    Md = Squash * Mdr * Mdf * Squash
-
     # field prior mask
     if iseven(nside)
         Ml = ones(Complex{T},nside÷2+1,nside)
@@ -76,19 +69,27 @@ function run2(;
     end
     Mf = Squash * Mff * Squash
     
+    # data mask
+    Mdf = Mff*FullDiagOp(F̂{T,P}(repeated(Cℓ_2D(P,1:ℓmax_data,ones(ℓmax_data)),nF)...))
+    if mask!=nothing
+        Mdr = FullDiagOp(F{T,P}(repeated(T.(sptlike_mask(nside,Θpix; (mask==true?():mask)...)),nF)...))
+    else
+        Mdr = 1
+    end
+    Md = Squash * Mdr * Mdf * Squash
     # ϕ prior mask
     Mϕ = Squash
     
     ## simulate data
     seed!=nothing && srand(seed)
-    f = simulate(Cf)
+    f = simulate(Cf_data)
     ϕ = simulate(Cϕ)
     f̃ = L(ϕ)*f
     d = f̃ + simulate(Cn)
 
     target_lnP = mean(let n=simulate(Cn); -n⋅(Md'*(Cn\(Md*n)))/2 end for i=1:100)
     @show target_lnP
-    rundat = @dictpack Θpix nside T r μKarcminT d target_lnP Cℓf Cℓn f f̃ ϕ
+    rundat = @dictpack Θpix nside T r r_data μKarcminT d target_lnP Cℓf Cℓf_data Cℓn f f̃ ϕ
 
     trace = []
     ϕcur = 0ϕ
@@ -107,7 +108,7 @@ function run2(;
         
         ds = DataSet(d, Cn, Cfw, Cϕ, Md, Mf, Mϕ)
         
-        let L = (w==0?IdentityOp:L(ϕcur)),
+        let L = (w==0?IdentityOp:cache(L(ϕcur))),
             P = nan2zero.(sqrtm((nan2zero.(Mdf * Cn^-1) .+ nan2zero.(Mff * Cfw^-1)))^-1)
             A = L'*(Md'*(Cn^-1)*Md*L) + Mf'*Cfw^-1*Mf
             b = L'*(Md'*(Cn^-1)*Md*d)
@@ -115,14 +116,24 @@ function run2(;
             f̃cur = L*fcur
         end
 
-        ϕnew = Mϕ*Cϕ*(δlnΠᶠ_δfϕ(fcur,ϕcur,ds) * δfϕ_δf̃ϕ(L(ϕcur),fcur,f̃cur))[2]
-        α = (res = optimize(α->(-lnP(1,f̃cur,(1-α)*ϕcur+α*ϕnew,ds,L)), T(0), T(1), abs_tol=1e-6)).minimizer
-        ϕcur = (1-α)*ϕcur+α*ϕnew
-
-        lnPw = -res.minimum
-        lnP1 = lnP(1,f̃cur,(1-α)*ϕcur+α*ϕnew,DataSet(d, Cn, Cf, Cϕ, Md, Mf, Mϕ),L)
-        push!(trace,@dictpack f̃cur fcur ϕcur ϕnew lnPw lnP1 α w hist)
-        @printf("%i %.4f %.2f %.2f %i %.4f\n",i,w500,lnPw,lnP1,length(hist),α)
+        local α,ϕnew,lnPw
+        if i!=endof(ws)
+            ϕnew = Mϕ*Cϕ*(δlnΠᶠ_δfϕ(LP(ℓmax_masking_hack)*fcur,ϕcur,ds) * δfϕ_δf̃ϕ(L(ϕcur),fcur,f̃cur))[2]
+            α = (res = optimize(α->(-lnP(1,f̃cur,(1-α)*ϕcur+α*ϕnew,ds,L)), T(0), T(1), abs_tol=1e-6)).minimizer
+            ϕcur = (1-α)*ϕcur+α*ϕnew
+            lnPw = -res.minimum
+        end
+        
+        lnP1 = lnP(1,f̃cur,ϕcur,DataSet(d, Cn, Cf, Cϕ, Md, Mf, Mϕ),L)
+        
+        if i!=endof(ws)
+            push!(trace, @dictpack Cfw f̃cur fcur ϕcur ϕnew lnPw lnP1 α hist w)
+            @printf("%i %.4f %.2f %.2f %i %.4f\n",i,w500,lnPw,lnP1,length(hist),α)
+        else
+            push!(trace, @dictpack Cfw f̃cur fcur ϕcur lnPw=>lnP1 lnP1 hist w)
+            @printf("%i %.4f %.2f %i\n",i,w500,lnP1,length(hist))
+        end
+        if w==:auto; trace[end][:wℓ]=wℓ; end
         
         outfile!=nothing && save(outfile,"rundat",rundat,"trace",trace)
             
