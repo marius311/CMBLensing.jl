7095ae18ecb6f947ce22a6f8b397cdb75978dfcb
marius <mmillea@ucdavis.edu>
2017-07-10 10:18:04 +0200

    improve plotting

--- src/plotting.jl
+++ src/plotting.jl
@@ -7,17 +7,47 @@ pretty_name(s::Symbol) = pretty_name(string(s))
 pretty_name(s::String) = s[1:1]*" "*(Dict('x'=>"map",'l'=>"fourier")[s[2]])
 
 # generic plotting some components of a FlatField
-function plot(f::FlatField{T,P}, axs, which; kwargs...) where {T,Θ,N,P<:Flat{Θ,N}}
+function plot(f::FlatField{T,P}, axs, which; units=:deg, ticklabels=true, kwargs...) where {T,Θ,N,P<:Flat{Θ,N}}
+    x = Θ*N/Dict(:deg=>60,:arcmin=>1)[units]/2
+    extent = [-x,x,-x,x]
     for (ax,k) in zip(axs,which)
-        ax = plot(f[k]; ax=ax, title="$(pretty_name(k)) ($(N)x$(N) @ $(Θ)')", kwargs...)
+        plot(f[k]; ax=ax, extent=extent, title="$(pretty_name(k)) ($(N)x$(N) @ $(Θ)')", kwargs...)
+        if ticklabels
+            @pydef type MyFmt <: pyimport(:matplotlib)[:ticker][:ScalarFormatter]
+                __call__(self,v,p=nothing) = py"super"(MyFmt,self)[:__call__](v,p)*Dict(:deg=>"°",:arcmin=>"′")[units]
+            end
+            ax[:xaxis][:set_major_formatter](MyFmt())
+            ax[:yaxis][:set_major_formatter](MyFmt())
+            ax[:set_ylabel]("Dec")
+            ax[:set_xlabel]("RA")
+            ax[:tick_params](labeltop=false, labelbottom=true)
+        else
+            ax[:tick_params](labeltop=false, labelleft=false)
+        end
     end
 end
 
 # plotting a real matrix
-function plot(m::AbstractMatrix{<:Real}; title=nothing, kwargs...)
+function plot(m::AbstractMatrix{<:Real}; ax=gca(), title=nothing, vlim=:sym, cmap="RdBu_r", cbar=true, kwargs...)
+    
+    # some logic to automatically get upper/lower limits
+    if vlim==:sym
+        vmax = quantile(abs.(m[!isnan.(m)][:]),0.999)
+        vmin = -vmax
+    elseif vlim==:asym
+        vmin, vmax = (quantile(m[!isnan.(m)][:],q) for q=(0.001,0.999))
+    elseif isa(vlim,Tuple)
+        vmin, vmax = vlim
+    else
+        vmax = vlim
+        vmin = -vmax
+    end
+       
     m[isinf(m)]=NaN
-    ax = pyimport(:seaborn)[:heatmap](m; mask=isnan(m), xticklabels=false, yticklabels=false, square=true, kwargs...)
-    title!=nothing && ax[:set_title](title)
+    
+    cax = ax[:matshow](m; vmin=vmin, vmax=vmax, cmap=cmap, rasterized=true, kwargs...)
+    cbar && gcf()[:colorbar](cax,ax=ax)
+    title!=nothing && ax[:set_title](title, y=1)
     ax
 end
 
@@ -30,18 +60,20 @@ function plot(m::AbstractMatrix{Complex{T}}; kwargs...) where {T}
 end
 
 # FlatS0
-function plot(fs::AbstractMatrix{<:FlatS0}; plotsize=4, which=[:Tx], kwargs...)
+function plot(fs::AbstractMatrix{<:FlatS0}; plotsize=4.8, which=[:Tx], kwargs...)
     (length(which)==1) || throw(ArgumentError("Can't plot matrix of FlatS0's with multiple components, $(which)"))
-    fig,axs = subplots(size(fs,1), size(fs,2); figsize=plotsize.*(size(fs,2),size(fs,1)), squeeze=false)
+    fig,axs = subplots(size(fs)...; figsize=plotsize.*[1.4*size(fs,2),size(fs,1)], squeeze=false)
     for i=eachindex(fs)
         plot(fs[i], [axs[i]], which; kwargs...)
     end
+    fig,axs
 end
 function plot(fs::AbstractVector{<:FlatS0}; plotsize=4, which=[:Tx], kwargs...)
     fig,axs = subplots(length(fs), length(which); figsize=plotsize.*(length(which),length(fs)), squeeze=false)
     for i=1:length(fs)
         plot(fs[i], axs[i,:], which; kwargs...)
     end
+    fig,axs
 end
 plot(f::FlatS0; kwargs...) = plot([f]; kwargs...)
 
@@ -67,5 +99,13 @@ function plot(fs::AbstractVector{<:Field2Tuple{<:FlatS0,<:FlatS2}}; plotsize=4, 
     for i=1:length(fs)
         plot(fs[i], axs[i,:], which; kwargs...)
     end
+    fig,axs
+end
+function plot(fs::RowVector{<:Field2Tuple{<:FlatS0,<:FlatS2}}; plotsize=4.8, which=[:Tx,:Ex,:Bx], kwargs...)
+    fig,axs = subplots(length(which), length(fs); figsize=(plotsize.*[1.4length(fs),length(which)]), squeeze=false)
+    for i=1:length(fs)
+        plot(fs[i], axs[:,i], which; kwargs...)
+    end
+    fig,axs
 end
 plot(f::Field2Tuple{<:FlatS0,<:FlatS2}; kwargs...) = plot([f]; kwargs...)
