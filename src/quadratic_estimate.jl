02df4813e34395f308d90c34ab2be8b062ce6508
marius <mmillea@ucdavis.edu>
2017-11-22 23:50:40 +0100

    add EB quadratic estimate

--- src/quadratic_estimate.jl
+++ src/quadratic_estimate.jl
@@ -1,29 +1,82 @@
-export ϕqeTT
+export ϕqe
+
+using Combinatorics: levicivita
 
 sinv(x) = nan2zero.(1./x)
 
-"""Compute quadratic estimate for ϕ given TT data, d, and signal, lensed signal, and noise covariances Cf, Cf̃, and Cn"""
-function ϕqeTT(d::FlatS0, Cf, Cf̃, Cn, Cϕ=nothing; WF=false)
+"""
+Compute quadratic estimate for ϕ given data, d, and signal, lensed signal,
+and noise covariances Cf, Cf̃, and Cn, respectively. If signal covariance Cϕ is
+given, the result is Wiener filtered.
+"""
+# XX estimator where X∈[T,E,B]
+function ϕqe(d::FlatS0, Cf, Cf̃, Cn, Cϕ=nothing)
 
-    L̅  = (@SVector [∇[1] .+ 0Ð(d), ∇[2] .+ 0Ð(d)]);
-    L² = FullDiagOp(L̅'L̅)
+    L̅,L² = get_L̅_L²(d)
 
     # quadratic estimate
-    dL = L̅' * Fourier(Map(inv(Cf̃+Cn)*d) * Map(L̅*(Cf*inv(Cf̃+Cn)*d)))
+    ∫d²l = L̅' * Fourier(Map(inv(Cf̃+Cn)*d) * Map(L̅*(Cf*inv(Cf̃+Cn)*d)))
     
     # normalization
     term1 = Map((Cf^2)*inv(Cf̃+Cn).*(L̅*L̅')) .* Map(  sinv(Cf̃+Cn).f )
     term2 = Map( Cf   *inv(Cf̃+Cn).* L̅    ) .* Map(Cf*inv(Cf̃+Cn).*L̅)'
     AL = 2π * L² * sinv(L̅' * Fourier(term1 + term2) * L̅)
     
-    ϕest = -AL*(L²\dL)
+    ϕL = -AL*(L²\∫d²l)
     
-    if WF
-        @assert Cϕ != nothing "Need to provide Cϕ if WF=true"
+    if Cϕ != nothing
         Nϕ = FullDiagOp(abs.(AL*inv(L²)))
-        Cϕ * inv(Cϕ + Nϕ) * ϕest
+        Cϕ * inv(Cϕ + Nϕ) * ϕL
     else
-        ϕest
+        ϕL
     end
 
+end
+
+# EB estimator, currently assuming B=0 (todo: remove this assumption)
+function ϕqe(d::FlatS2{T,P}, Cf, Cf̃, Cn, Cϕ=nothing) where {T,P}
+
+    L̅,L² = get_L̅_L²(d[:E])
+
+    ϵ(x...) = levicivita([x...])
+    
+    getCEB(C) = FullDiagOp(FlatS0Fourier{T,P}(C.f.El)), FullDiagOp(FlatS0Fourier{T,P}(C.f.Bl))
+    CE,CB   = getCEB(Cf)
+    CẼ,CB̃   = getCEB(Cf̃)
+    CEn,CBn = getCEB(Cn)
+
+    # quadratic estimate
+    @sym E(i,j,k) = Map(L²\L̅[i]*L̅[j]*L̅[k]*(CE*inv(CẼ+CEn)*d[:E]))
+    @sym B(i,j)   = Map(L²\L̅[i]*L̅[j]        *(inv(CB̃+CBn)*d[:B]))
+    ∫d²l = sum(L̅[i] * Fourier(sum(ϵ(k,m,3) * E(i,j,k) * B(j,m) for j=1:2,k=1:2,m=1:2)) for i=1:2)
+    
+    # normalization
+    @sym E2(i,j,k,m) = Map(L²\L̅[i]*L̅[j]*L̅[k]*L̅[m]*(CE.^2*inv(CẼ+CEn)))
+    @sym B2(i,j)     = Map(L²\L̅[i]*L̅[j]                *(inv(CB̃+CBn)))
+    AL = π * L² * sinv(sum(L̅[i]*L̅[j] * Fourier(sum(ϵ(k,n,3) * ϵ(m,p,3) * E2(i,j,k,m) * B2(n,p) for k=1:2,m=1:2,n=1:2,p=1:2)) for i=1:2,j=1:2))
+    
+    ϕL = AL*(L²\∫d²l)
+    
+    if Cϕ != nothing
+        Nϕ = FullDiagOp(abs.(AL*inv(L²)))
+        Cϕ * inv(Cϕ + Nϕ) * ϕL
+    else
+        ϕL
+    end
+    
+end
+
+function get_L̅_L²(f)
+    # todo: make L automatically broadcast generically like ∇ does
+    L̅  = (@SVector [∇[1] .+ 0Ð(f), ∇[2] .+ 0Ð(f)])
+    L² = FullDiagOp(L̅'L̅)
+    L̅,L²
+end
+
+
+macro sym(ex)
+    # todo: write this such that it converts a function f(i,j,k,..) which is
+    # assumed to be symmetric in its arguments into a memoized function such
+    # that each unique set of parameters is only calculated once
+    :($(esc(ex)))
 end
