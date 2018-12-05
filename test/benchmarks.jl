46969fdc6db7d9af144049192ab01a0ab8029f47
marius <mmillea@ucdavis.edu>
2018-12-05 14:14:21 -0800

    update benchmarking

--- test/benchmarks.jl
+++ test/benchmarks.jl
@@ -1,34 +1,26 @@
 using CMBLensing
-using CMBLensing: jrk4, cache, @dictpack
+using CMBLensing: jrk4, cache, @dictpack, fourier∂
 using BenchmarkTools
 
 ##
 T = Float32
 nside = 256
-P = Flat{3,nside}
+P = Flat{3,nside,fourier∂}
 ϕ = FlatS0Map{T,P}(randn(nside,nside))/1e7
-f = FlatS2QUMap{T,P}(@repeated(randn(nside,nside),2)...)
-L = LenseFlow{jrk4{7}}(ϕ)
-cL = cache(L)
+f = FlatS0Map{T,P}(@repeated(randn(nside,nside),1)...)
+L = cache(LenseFlow{jrk4{7}}(ϕ),f)
 J = δf̃ϕ_δfϕ(L,L*f,f)
-cJ = δf̃ϕ_δfϕ(cL,cL*f,f)
 fϕ = FieldTuple(f,ϕ)
 # some fake data and covariances to time the likelihood gradient
-# d = f
-# Cn,Cf,Cf̃ = @repeated(FullDiagOp(FlatS2QUMap{T,P}(@repeated(randn(nside,nside),2)...)),3)
-# Cϕ = FullDiagOp(FlatS0Map{T,P}(randn(nside,nside))/1e7)
-# ds = DataSet(;@dictpack(d,Cn,Cf,Cf̃,Cϕ)...)
+d = f
+Cn,Cf,Cf̃ = @repeated(FullDiagOp(FlatS0Map{T,P}(@repeated(rand(nside,nside),1)...)),3)
+Cϕ = FullDiagOp(FlatS0Map{T,P}(rand(nside,nside))/1e7)
+ds = DataSet(;@dictpack(d,Cn,Cf,Cf̃,Cϕ)...)
 ##
-myshow(s) = (print_with_color(:light_green,"=== "*s*" ===\n"); t->(show(STDOUT,MIME("text/plain"),t); println()))
+myshow(s) = (print_with_color(:light_green,"=== "*s*" ===\n"); t->(show(stdout,MIME("text/plain"),t); println()))
 ##
-@benchmark($L * $f)   |> myshow("Lense")
-@benchmark($f * $L)   |> myshow("Transpose Lense")
-@benchmark($J * $fϕ)  |> myshow("Jacobian")
-@benchmark($fϕ * $J)  |> myshow("Transpose Jacobian")
-##
-@benchmark($cL * $f)  |> myshow("Cached Lense")
-@benchmark($f * $cL)  |> myshow("Cached Transpose Lense")
-@benchmark($cJ * $fϕ) |> myshow("Cached Jacobian")
-@benchmark($fϕ * $cJ) |> myshow("Cached Transpose Jacobian")
-##
-@benchmark(δlnP_δfϕₜ(:mix, $f, $ϕ, $ds, $cL)) |> myshow("Cached mixed posterior gradient")
+@benchmark(cache(LenseFlow{jrk4{7}}($ϕ),$f)) |> myshow("Caching")
+@benchmark($L    * $f)                       |> myshow("Lense")
+@benchmark($(L') * $f)                       |> myshow("Transpose Lense")
+@benchmark($(J') * $fϕ)                      |> myshow("Transpose Jacobian")
+@benchmark(δlnP_δfϕₜ(:mix, $f, $ϕ, $ds, $L)) |> myshow("Mixed posterior gradient")
