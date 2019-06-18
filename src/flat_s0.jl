0a2078762b0251171f7c24028f9a24a8f0c676b4
marius <mariusmillea@gmail.com>
2019-06-18 14:54:25 -0700

    possible FieldTuple prototype

--- src/flat_s0.jl
+++ src/flat_s0.jl
@@ -2,60 +2,67 @@
 # this file defines a flat-sky pixelized spin-0 map (like T or ϕ)
 # and operators on this map
 
-export FlatS0Fourier, FlatS0Map, FlatS0
+export FlatFourier, FlatMap, FlatS0
 
-struct FlatS0Map{P<:Flat,T,M<:AbstractMatrix{T}} <: Field{Map,S0,P,T}
+
+## FlatMap and FlatFourier types
+struct FlatMap{P<:Flat,T<:Real,M<:AbstractMatrix{T}} <: Field{Map,S0,P,T}
     Ix :: M
 end
-
-struct FlatS0Fourier{P<:Flat,T,M<:AbstractMatrix{Complex{T}}} <: Field{Fourier,S0,P,Complex{T}}
+struct FlatFourier{P<:Flat,T<:Real,M<:AbstractMatrix{Complex{T}}} <: Field{Fourier,S0,P,Complex{T}}
     Il :: M
 end
+const FlatS0{P,T,M} = Union{FlatMap{P,T,M},FlatFourier{P,T,M}}
 
-size(f::FlatField) = (length(f.Ix),)
-@propagate_inbounds @inline getindex(f::FlatS0Map, I...) = getindex(f.Ix, I...)
-@propagate_inbounds @inline setindex!(f::FlatS0Map, X, I...) = setindex!(f.Ix, X, I...)
+## convenience constructors
+FlatMap(Ix; θpix=θpix₀, ∂mode=fourier∂) = FlatMap{Flat{size(Ix,2),θpix,∂mode}}(Ix)
+FlatMap{P}(Ix::M) where {P,T,M<:AbstractMatrix{T}} = FlatMap{P,T,M}(Ix)
+FlatFourier(Il; θpix=θpix₀, ∂mode=fourier∂) = FlatFourier{Flat{size(Il,2),θpix,∂mode}}(Il)
+FlatFourier{P}(Il::M) where {P,T,M<:AbstractMatrix{Complex{T}}} = FlatFourier{P,T,M}(Il)
 
+## pretty printing
+Base.print_array(io::IO, f::FlatS0) = Base.print_array(io, broadcast_data(f)[:])
+Base.summary(io::IO, f::F) where {N,θpix,∂mode,F<:FlatS0{Flat{N,θpix,∂mode}}} = 
+print(io, "$(length(f))-element $(F.name.name){$N×$N map, $(θpix)′ pixels, $(∂mode.name.name)}")
 
-BroadcastStyle(::Type{F}) where {F<:FlatS0Map} = ArrayStyle{F}()
-similar(bc::Broadcasted{ArrayStyle{F}}, ::Type{T}) where {T, N, P<:Flat{N}, F<:FlatS0Map{P}} = FlatS0Map{P}(similar(Array{T}, N, N))
+## array interface 
+size(f::FlatS0) = (length(broadcast_data(f)),)
+@propagate_inbounds @inline getindex(f::FlatS0, I...) = getindex(broadcast_data(f), I...)
+@propagate_inbounds @inline setindex!(f::FlatS0, X, I...) = setindex!(broadcast_data(f), X, I...)
+
+## broadcasting
+BroadcastStyle(::Type{F}) where {F<:FlatMap} = ArrayStyle{F}()
+similar(bc::Broadcasted{ArrayStyle{F}}, ::Type{T}) where {T, N, P<:Flat{N}, F<:FlatMap{P}} = FlatMap{P}(similar(Array{T}, N, N))
 function Broadcast.preprocess(dest::F, bc::Broadcasted{Nothing}) where {F<:Field}
     bc′ = Broadcast.flatten(bc)
     Broadcasted{Nothing}(bc′.f, map(arg->broadcast_data(F,arg), bc′.args), axes(dest))
 end
-@inline field_data(f::FlatS0Map) = f.Ix
-@inline field_data(x) = x
+broadcast_data(::Any, f) = f
+broadcast_data(f::FlatS0) = first(fieldvalues(f))
 
 
-const FlatS0{P,T,M}=Union{FlatS0Map{P,T,M},FlatS0Fourier{P,T,M}}
-
-# convenience constructors
-FlatS0Map(Ix; θpix=θpix₀, ∂mode=fourier∂) = FlatS0Map{Flat{size(Ix,2),θpix,∂mode}}(Ix)
-FlatS0Map{P}(Ix::M) where {P,T,M<:AbstractMatrix{T}} = FlatS0Map{P,T,M}(Ix)
-FlatS0Fourier(Tl::Matrix{Complex{T}}; θpix=θpix₀, ∂mode=fourier∂) where {T, M<:AbstractMatrix{Complex{T}}} = FlatS0Fourier{Flat{size(Tl,2),θpix,∂mode},T,M}(Tl)
-FlatS0Fourier{P}(Ix::M) where {P,T,M<:AbstractMatrix{T}} = FlatS0Map{P,T,M}(Ix)
 
 
 # 
 # # convenience conversion funtions:
-# Fourier(f::FlatS0Map{T,P}) where {T,P} = FlatS0Fourier{T,P}(ℱ{P}*f.Ix)
-# Map(f::FlatS0Fourier{T,P}) where {T,P} = FlatS0Map{T,P}(ℱ{P}\f.Tl)
+# Fourier(f::FlatMap{T,P}) where {T,P} = FlatFourier{T,P}(ℱ{P}*f.Ix)
+# Map(f::FlatFourier{T,P}) where {T,P} = FlatMap{T,P}(ℱ{P}\f.Tl)
 # 
 # # inplace conversions
-# Fourier(f′::FlatS0Fourier{T,P}, f::FlatS0Map{T,P}) where {T,P} = (mul!(f′.Tl,  FFTgrid(T,P).FFT, f.Ix); f′)
-# Map(f′::FlatS0Map{T,P}, f::FlatS0Fourier{T,P}) where {T,P}     = (ldiv!(f′.Ix, FFTgrid(T,P).FFT, f.Tl); f′)
+# Fourier(f′::FlatFourier{T,P}, f::FlatMap{T,P}) where {T,P} = (mul!(f′.Tl,  FFTgrid(T,P).FFT, f.Ix); f′)
+# Map(f′::FlatMap{T,P}, f::FlatFourier{T,P}) where {T,P}     = (ldiv!(f′.Ix, FFTgrid(T,P).FFT, f.Tl); f′)
 # 
 # 
 # LenseBasis(::Type{<:FlatS0}) = Map
 # 
 # function white_noise(::Type{F}) where {Θ,Nside,T,P<:Flat{Θ,Nside},F<:FlatS0{T,P}}
-#     FlatS0Map{T,P}(randn(Nside,Nside) / FFTgrid(T,P).Δx)
+#     FlatMap{T,P}(randn(Nside,Nside) / FFTgrid(T,P).Δx)
 # end
 # 
 # """ Convert power spectrum Cℓ to a flat sky diagonal covariance """
 # function Cℓ_to_cov(::Type{T}, ::Type{P}, ::Type{S0}, CℓTT::InterpolatedCℓs) where {T,P}
 #     g = FFTgrid(T,P)
-#     FullDiagOp(FlatS0Fourier{T,P}(Cℓ_2D(CℓTT.ℓ, CℓTT.Cℓ, g.r)[1:g.nside÷2+1,:]))
+#     FullDiagOp(FlatFourier{T,P}(Cℓ_2D(CℓTT.ℓ, CℓTT.Cℓ, g.r)[1:g.nside÷2+1,:]))
 # end
 # 
 # function cov_to_Cℓ(L::FullDiagOp)
@@ -78,13 +85,13 @@ FlatS0Fourier{P}(Ix::M) where {P,T,M<:AbstractMatrix{T}} = FlatS0Map{P,T,M}(Ix)
 #     InterpolatedCℓs(bandcenters ./ counts,  power ./ counts ./ α)
 # end
 # 
-# zero(::Type{<:FlatS0{T,P}}) where {T,P} = FlatS0Map{T,P}(zeros(Nside(P),Nside(P)))
-# one(::Type{<:FlatS0Map{T,P}}) where {T,P} = FlatS0Map{T,P}(ones(Nside(P),Nside(P)))
-# one(::Type{<:FlatS0Fourier{T,P}}) where {T,P} = FlatS0Fourier{T,P}(ones(Complex{T},Nside(P)÷2+1,Nside(P)))
+# zero(::Type{<:FlatS0{T,P}}) where {T,P} = FlatMap{T,P}(zeros(Nside(P),Nside(P)))
+# one(::Type{<:FlatMap{T,P}}) where {T,P} = FlatMap{T,P}(ones(Nside(P),Nside(P)))
+# one(::Type{<:FlatFourier{T,P}}) where {T,P} = FlatFourier{T,P}(ones(Complex{T},Nside(P)÷2+1,Nside(P)))
 # 
 # # dot products
-# dot(a::FlatS0Map{T,P}, b::FlatS0Map{T,P}) where {T,P} = dot(a.Ix,b.Ix) * FFTgrid(T,P).Δx^2
-# dot(a::FlatS0Fourier{T,P}, b::FlatS0Fourier{T,P}) where {T,P} = begin
+# dot(a::FlatMap{T,P}, b::FlatMap{T,P}) where {T,P} = dot(a.Ix,b.Ix) * FFTgrid(T,P).Δx^2
+# dot(a::FlatFourier{T,P}, b::FlatFourier{T,P}) where {T,P} = begin
 #     @unpack nside,Δℓ = FFTgrid(T,P)
 #     if isodd(nside)
 #         @views real(2 * (a.Tl[2:end,:][:] ⋅ b.Tl[2:end,:][:]) + (a.Tl[1,:][:] ⋅ b.Tl[1,:][:])) * Δℓ^2
@@ -97,10 +104,10 @@ FlatS0Fourier{P}(Ix::M) where {P,T,M<:AbstractMatrix{T}} = FlatS0Map{P,T,M}(Ix)
 # 
 # # vector conversion
 # length(::Type{<:FlatS0{T,P}}) where {T,P} = Nside(P)^2
-# getindex(f::FlatS0Map,::Colon) = f.Ix[:]
-# getindex(f::FlatS0Fourier,::Colon) = rfft2vec(f.Tl)
-# fromvec(::Type{FlatS0Map{T,P}},     vec::AbstractVector) where {T,P} = FlatS0Map{T,P}(reshape(vec,(Nside(P),Nside(P))))
-# fromvec(::Type{FlatS0Fourier{T,P}}, vec::AbstractVector) where {T,P} = FlatS0Fourier{T,P}(vec2rfft(vec))
+# getindex(f::FlatMap,::Colon) = f.Ix[:]
+# getindex(f::FlatFourier,::Colon) = rfft2vec(f.Tl)
+# fromvec(::Type{FlatMap{T,P}},     vec::AbstractVector) where {T,P} = FlatMap{T,P}(reshape(vec,(Nside(P),Nside(P))))
+# fromvec(::Type{FlatFourier{T,P}}, vec::AbstractVector) where {T,P} = FlatFourier{T,P}(vec2rfft(vec))
 # 
 # 
 # 
@@ -137,23 +144,23 @@ FlatS0Fourier{P}(Ix::M) where {P,T,M<:AbstractMatrix{T}} = FlatS0Map{P,T,M}(Ix)
 #         # downgrade
 #         if anti_aliasing
 #             kmask = ifelse.(abs.(FFTgrid(T,P).k) .> FFTgrid(T,Pnew).nyq, 0, 1)
-#             AA = FullDiagOp(FlatS0Fourier{T,P}(kmask[1:N÷2+1] .* kmask'))
+#             AA = FullDiagOp(FlatFourier{T,P}(kmask[1:N÷2+1] .* kmask'))
 #         else
 #             AA = 1
 #         end
 #         if mode==:map
-#             fnew = FlatS0Map{T,Pnew}(mapslices(mean,reshape((AA*f)[:Ix],(fac,Nnew,fac,Nnew)),dims=(1,3))[1,:,1,:])
-#             deconv_pixwin ? FlatS0Fourier{T,Pnew}(fnew[:Tl] ./ Wk' ./ Wk[1:Nnew÷2+1]) : fnew
+#             fnew = FlatMap{T,Pnew}(mapslices(mean,reshape((AA*f)[:Ix],(fac,Nnew,fac,Nnew)),dims=(1,3))[1,:,1,:])
+#             deconv_pixwin ? FlatFourier{T,Pnew}(fnew[:Tl] ./ Wk' ./ Wk[1:Nnew÷2+1]) : fnew
 #         else
-#             FlatS0Fourier{T,Pnew}((AA*f)[:Tl][1:(Nnew÷2+1), [1:(isodd(Nnew) ? Nnew÷2+1 : Nnew÷2); (end-Nnew÷2+1):end]])
+#             FlatFourier{T,Pnew}((AA*f)[:Tl][1:(Nnew÷2+1), [1:(isodd(Nnew) ? Nnew÷2+1 : Nnew÷2); (end-Nnew÷2+1):end]])
 #         end
 #     else
 #         # upgrade
 #         if mode==:map
-#             fnew = FlatS0Map{T,Pnew}(hvcat(N,(x->fill(x,(fac,fac))).(f.Ix)...)')
-#             deconv_pixwin ? FlatS0Fourier{T,Pnew}(fnew[:Tl] .* Wk' .* Wk[1:Nnew÷2+1]) : fnew
+#             fnew = FlatMap{T,Pnew}(hvcat(N,(x->fill(x,(fac,fac))).(f.Ix)...)')
+#             deconv_pixwin ? FlatFourier{T,Pnew}(fnew[:Tl] .* Wk' .* Wk[1:Nnew÷2+1]) : fnew
 #         else
-#             fnew = Fourier(zero(FlatS0Map{T,Pnew}))
+#             fnew = Fourier(zero(FlatMap{T,Pnew}))
 #             broadcast_setindex!(fnew.Tl, f[:Tl], 1:(N÷2+1), [findfirst(FFTgrid(fnew).k .≈ FFTgrid(f).k[i]) for i=1:N]');
 #             fnew
 #         end
