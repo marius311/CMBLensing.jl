d4da0e79189119a8f2773c74be814a03d8568ef5
marius <mariusmillea@gmail.com>
2019-12-15 23:34:26 +0100

    update docs

--- make.jl
+++ make.jl
@@ -1,10 +1,12 @@
-using CMBLensing
+# ensure in right directory and environment
+cd(dirname(@__FILE__))
+using Pkg
+pkg"activate ."
 
 ##
 
+using CMBLensing
 using Documenter
-# ensure in right directory
-cd(dirname(@__FILE__))
 
 # convert Jupyter notebooks to markdown and do some simple preprocessing before
 # feeding into Documenter.jl
@@ -46,50 +48,102 @@ end
 
 # adds the MyBinder button on each page that is a notebook
 
-@eval Documenter.Writers.HTMLWriter function render_article(ctx, navnode)
-    @tags article header footer nav ul li hr span a img
+@eval Documenter.Writers.HTMLWriter function render_navbar(ctx, navnode, edit_page_link::Bool)
+    @tags div header nav ul li a span img
 
-    header_links = map(Documents.navpath(navnode)) do nn
+    # The breadcrumb (navigation links on top)
+    navpath = @show Documents.navpath(navnode)
+    header_links = map(navpath) do nn
         title = mdconvert(pagetitle(ctx, nn); droplinks=true)
-        nn.page === nothing ? li(title) : li(a[:href => navhref(ctx, nn, navnode)](title))
+        nn.page === nothing ? li(a[".is-disabled"](title)) : li(a[:href => navhref(ctx, nn, navnode)](title))
     end
+    header_links[end] = header_links[end][".is-active"]
+    breadcrumb = nav[".breadcrumb"]() ### modified to not show breadcrumbs
 
-    topnav = nav(ul(header_links))
-
-    pageurl = get(getpage(ctx, navnode).globals.meta, :EditURL, getpage(ctx, navnode).source)
-    nbpath = foldl(replace,["src-staging"=>"src",".md"=>".ipynb"], init=pageurl)
-    if isfile(nbpath)
-        url = "https://mybinder.org/v2/gh/marius311/CMBLensing.jl/master?urlpath=lab/tree/$(basename(nbpath))"
-        push!(topnav.nodes, a[".edit-page", :href => url](img[:src => "https://mybinder.org/badge_logo.svg"]()))
-    end
+    # The "Edit on GitHub" links and the hamburger to open the sidebar (on mobile) float right
+    navbar_right = div[".docs-right"]
     
-    art_header = header(topnav, hr(), render_topbar(ctx, navnode))
+    ### custom code to add MyBinder link
+    if edit_page_link
+        pageurl = get(getpage(ctx, navnode).globals.meta, :EditURL, getpage(ctx, navnode).source)
+        nbpath = foldl(replace,["src-staging"=>"src",".md"=>".ipynb"], init=pageurl)
+        if @show isfile(nbpath)
+            url = "https://mybinder.org/v2/gh/marius311/CMBLensing.jl/master?urlpath=lab/tree/$(basename(nbpath))"
+            push!(navbar_right.nodes, a[".docs-right", :href => url](img[:src => "https://mybinder.org/badge_logo.svg"]()))
+        end
+    end
+    ###
+    
+    # Set the logo and name for the "Edit on.." button.
+    if edit_page_link && (ctx.settings.edit_link !== nothing) && !ctx.settings.disable_git
+        host_type = Utilities.repo_host_from_url(ctx.doc.user.repo)
+        if host_type == Utilities.RepoGitlab
+            host = "GitLab"
+            logo = "\uf296"
+        elseif host_type == Utilities.RepoGithub
+            host = "GitHub"
+            logo = "\uf09b"
+        elseif host_type == Utilities.RepoBitbucket
+            host = "BitBucket"
+            logo = "\uf171"
+        else
+            host = ""
+            logo = "\uf15c"
+        end
+        hoststring = isempty(host) ? " source" : " on $(host)"
 
-    # build the footer with nav links
-    art_footer = footer(hr())
-    if navnode.prev !== nothing
-        direction = span[".direction"]("Previous")
-        title = span[".title"](mdconvert(pagetitle(ctx, navnode.prev); droplinks=true))
-        link = a[".previous", :href => navhref(ctx, navnode.prev, navnode)](direction, title)
-        push!(art_footer.nodes, link)
+        pageurl = get(getpage(ctx, navnode).globals.meta, :EditURL, getpage(ctx, navnode).source)
+        
+        
+        edit_branch = isa(ctx.settings.edit_link, String) ? ctx.settings.edit_link : nothing
+        url = if Utilities.isabsurl(pageurl)
+            pageurl
+        else
+            if !(pageurl == getpage(ctx, navnode).source)
+                # need to set users path relative the page itself
+                pageurl = joinpath(first(splitdir(getpage(ctx, navnode).source)), pageurl)
+            end
+            Utilities.url(ctx.doc.user.repo, pageurl, commit=edit_branch)
+        end
+        if url !== nothing
+            edit_verb = (edit_branch === nothing) ? "View" : "Edit"
+            title = "$(edit_verb)$hoststring"
+            push!(navbar_right.nodes,
+                a[".docs-edit-link", :href => url, :title => title](
+                    span[".docs-icon.fab"](logo),
+                    span[".docs-label.is-hidden-touch"](title)
+                )
+            )
+        end
     end
 
-    if navnode.next !== nothing
-        direction = span[".direction"]("Next")
-        title = span[".title"](mdconvert(pagetitle(ctx, navnode.next); droplinks=true))
-        link = a[".next", :href => navhref(ctx, navnode.next, navnode)](direction, title)
-        push!(art_footer.nodes, link)
-    end
+    # Settings cog
+    push!(navbar_right.nodes, a[
+        "#documenter-settings-button.docs-settings-button.fas.fa-cog",
+        :href => "#", :title => "Settings",
+    ])
 
-    pagenodes = domify(ctx, navnode)
-    article["#docs"](art_header, pagenodes, art_footer)
+    # Hamburger on mobile
+    push!(navbar_right.nodes, a[
+        "#documenter-sidebar-button.docs-sidebar-button.fa.fa-bars.is-hidden-desktop",
+        :href => "#"
+    ])
+
+    # Construct the main <header> node that should be the first element in div.docs-main
+    header[".docs-navbar"](breadcrumb, navbar_right)
 end
+
+
+##
 
 makedocs(
     sitename="CMBLensing.jl", 
     source = "src-staging",
     build = "latest",
-    assets = ["assets/cmblensing.css"],
+    format = Documenter.HTML(
+        assets = ["assets/cmblensing.css"],
+        disable_git = true,
+    ),
     pages = [
         "index.md",
         "01_lense_a_map.md",
@@ -99,5 +153,4 @@ makedocs(
         "05_field_basics.md",
         "api.md"
     ],
-    html_disable_git = true
 )
