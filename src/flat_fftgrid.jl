4024b9cb8f4861a19aa099a562175c12a052faac
marius <mariusmillea@gmail.com>
2020-06-04 18:25:22 -0700

    add FFTW_TIMELIMIT option

--- src/flat_fftgrid.jl
+++ src/flat_fftgrid.jl
@@ -23,24 +23,27 @@ Nside(::Type{P}) where {N,P<:Flat{N}} = N
 
 
 @doc """
-The number of FFTW threads to use. This must be set via e.g.:
-
-    CMBLensing.FFTW_NUM_THREADS[] = 4
-
-*before* creating any `FlatField` objects; subsequent changes to this variable
-will be ignored. The default value is the environment variable `FFTW_NUM_THREADS`,
-or if that is not specified its `Sys.CPU_THREADS÷2`.
+The number of threads used by FFTW for CPU FFTs (default is the environment
+variable `FFTW_NUM_THREADS`, or if that is not specified its
+`Sys.CPU_THREADS÷2`). This must be set before creating any `FlatField` objects.
 """
-const FFTW_NUM_THREADS = Ref{Int}()
-@init FFTW_NUM_THREADS[] = parse(Int,get(ENV,"FFTW_NUM_THREADS","$(Sys.CPU_THREADS÷2)"))
+FFTW_NUM_THREADS = nothing
+@init global FFTW_NUM_THREADS = parse(Int,get(ENV,"FFTW_NUM_THREADS","$(Sys.CPU_THREADS÷2)"))
+
+
+@doc """
+Time-limit for FFT planning on CPU (default: 5 seconds). This must be set before
+creating any `FlatField` objects.
+"""
+FFTW_TIMELIMIT = 5
 
 
 function FlatInfo(T, Arr, θpix, Nside, D)
 
-    FFTW.set_num_threads(FFTW_NUM_THREADS[])
+    FFTW.set_num_threads(FFTW_NUM_THREADS)
 
     Δx   = T(deg2rad(θpix/60))
-    FFT  = plan_rfft(Arr{T}(undef,Nside,Nside,(D==1 ? () : (D,))...), (1,2))
+    FFT  = plan_rfft(Arr{T}(undef,Nside,Nside,(D==1 ? () : (D,))...), (1,2); (Arr <: Array ? (timelimit=FFTW_TIMELIMIT,) : ())...)
     Δℓ   = T(2π/(Nside*Δx))
     nyq  = T(2π/(2Δx))
     Ωpix = T(Δx^2)
