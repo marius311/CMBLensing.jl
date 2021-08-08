889dded6caae1eaaab0018b0d2435933b0986d60
marius <mariusmillea@gmail.com>
2021-08-08 13:24:21 -0700

    make fwdmodel return normal function

--- src/simpleppl.jl
+++ src/simpleppl.jl
@@ -5,20 +5,13 @@
 #   * [ ] handle ds object
 # * [ ] normalization terms
 # * [ ] mixing
+# * [ ] Union{Missing, T} for type args
 # * [x] _logpdf initial type?
 
+struct Simulate end
+struct Logpdf end
 
-struct SimplePPLModel{S,L}
-    simulate! :: S
-    logpdf! :: L
-end
-
-_simulate!(_vars, rng, m::SimplePPLModel, args...; kwargs...) = m.simulate!(_vars, rng, args...; kwargs...)
-_simulate!(_vars, rng, m,                 args...; kwargs...) = m(args...; kwargs...)
-
-_logpdf!(_logpdf, m::SimplePPLModel, args...; kwargs...) = m.logpdf!(_logpdf, args...; kwargs...)
-_logpdf!(_logpdf, m,                 args...; kwargs...) = m(args...; kwargs...)
-
+is_simpleppl_model(f) = false
 
 macro fwdmodel(def)
     sdef = splitdef(def)
@@ -26,61 +19,57 @@ macro fwdmodel(def)
     # simulate
     body_simulate = postwalk(sdef[:body]) do x
         if @capture(x, var_ ~ dist_)
-            quote
-                if ismissing($var)
-                    _vars[$(QuoteNode(var))] = $var = rand(rng, $dist)
-                else
-                    _vars[$(QuoteNode(var))] = $var
+            return :(ismissing($var) ? (_vars[$(QuoteNode(var))] = $var = rand(rng, $dist)) : (_vars[$(QuoteNode(var))] = $var))
+        elseif @capture(x, (f_(args__; kwargs__) | f_(args__)))
+            kwargs = kwargs == nothing ? () : kwargs
+            if isdefined(__module__, f)
+                if is_simpleppl_model(getfield(__module__, f))
+                    return :($f($(Simulate()),_vars, rng, $(args...); $(kwargs...)))
                 end
+            else
+                return :($is_simpleppl_model($f) ? $f($(Simulate()), _vars, rng, $(args...); $(kwargs...)) : $x)
             end
-        elseif @capture(x, (f_(args__; kwargs__) | f_(args__))) && !(isdefined(__module__, f) && (getfield(__module__, f) isa Base.Callable))
-            if kwargs == nothing
-                kwargs = ()
-            end
-            :($_simulate!(_vars, rng, $f, $(args...); $(kwargs...)))
-        else
-            x
         end
+        return x
     end
-    args_simulate = [[:_vars, :rng]; map(x -> Expr(:kw, x, missing), sdef[:args])]
-    def_simulate = combinedef(Dict(:args=>args_simulate, :kwargs=>sdef[:kwargs], :body=>body_simulate, :whereparams=>sdef[:whereparams]))
+    args_simulate = [[:(::$Simulate),:_vars, :rng]; map(x -> Expr(:kw, x, missing), sdef[:args])]
+    def_simulate = combinedef(Dict(:name=>sdef[:name], :args=>args_simulate, :kwargs=>sdef[:kwargs], :body=>body_simulate, :whereparams=>sdef[:whereparams]))
     
     # logpdf
     body_logpdf = postwalk(sdef[:body]) do x
         if @capture(x, var_ ~ dist_)
-            quote
-                _logpdf[] += logpdf($dist, $var)
-                $var
+            return :(_logpdf[] += logpdf($dist, $var); $var)
+        elseif @capture(x, (f_(args__; kwargs__) | f_(args__)))
+            kwargs = kwargs == nothing ? () : kwargs
+            if isdefined(__module__, f)
+                if is_simpleppl_model(getfield(__module__, f))
+                    return :($f($(Logpdf()),_logpdf, $(args...); $(kwargs...)))
+                end
+            else
+                return :($is_simpleppl_model($f) ? $f($(Logpdf()), _logpdf, $(args...); $(kwargs...)) : $x)
             end
-        elseif @capture(x, (f_(args__; kwargs__) | f_(args__))) && !(isdefined(__module__, f) && (getfield(__module__, f) isa Base.Callable))
-            if kwargs == nothing
-                kwargs = ()
-            end
-            :($_logpdf!(_logpdf, $f, $(args...); $(kwargs...)))
-        else
-            x
         end
+        return x
     end
-    args_logpdf = [[:_logpdf]; sdef[:args]]
-    def_logpdf = combinedef(Dict(:args=>args_logpdf, :kwargs=>sdef[:kwargs], :body=>body_logpdf, :whereparams=>sdef[:whereparams]))
+    args_logpdf = [[:(::$Logpdf),:_logpdf]; sdef[:args]]
+    def_logpdf = combinedef(Dict(:name=>sdef[:name], :args=>args_logpdf, :kwargs=>sdef[:kwargs], :body=>body_logpdf, :whereparams=>sdef[:whereparams]))
 
 
     esc(quote
-        $(sdef[:name]) = $SimplePPLModel(
-            $def_simulate,
-            $def_logpdf
-        )
+        $def_simulate
+        @eval $CMBLensing.is_simpleppl_model(::typeof($(Expr(:$, sdef[:name])))) = true
+        $def_logpdf
     end)
 end
 
-simulate(m::SimplePPLModel, args...; kwargs...) = simulate(Random.default_rng(), m, args...; kwargs...)
-function simulate(rng::AbstractRNG, m::SimplePPLModel, args...; kwargs...)
+simulate(model, args...; kwargs...) = simulate(Random.default_rng(), model, args...; kwargs...)
+function simulate(rng::AbstractRNG, model, args...; kwargs...)
     _vars = Dict()
-    retval = _simulate!(_vars, rng, m, args...; kwargs...)
+    retval = model(Simulate(),_vars, rng, args...; kwargs...)
     retval, (;_vars...)
 end
-function Distributions.logpdf(m::SimplePPLModel, args...; kwargs...)
+function Distributions.logpdf(model, args...; kwargs...)
     _logpdf = Ref{Real}(0)
-    _logpdf!(_logpdf, m, args...; kwargs...)
+    model(Logpdf(),_logpdf, args...; kwargs...)
     _logpdf[]
 end
