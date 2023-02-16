1883ab42c08d1378898b5fa08315128d7d470e81
marius <mariusmillea@gmail.com>
2023-02-15 18:21:26 -0800

    add precompile as preference

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,7 +1,17 @@
 
-for pol in (:I, :P, :IP)
-    for T in (Float32, Float64)
-        for storage in (Array,)
+let 
+    
+    precompile = @load_preference("precompile", default=false)
+
+    if precompile != false
+
+        if precompile == true
+            precompile_type_matrix = Iterators.product((:I,:P,:IP), (Float32,Float64), (Array,))
+        else
+            precompile_type_matrix = Iterators.product(eval(Meta.parse(precompile))...)
+        end
+
+        for (pol, T, storage) in precompile_type_matrix
             (;f, ϕ, ds) = load_sim(;pol, T, storage, Nside=16, θpix=1, seed=0)
             logpdf(ds; f, ϕ)
             (;f°, ϕ°) = mix(ds; f, ϕ)
@@ -9,7 +19,9 @@ for pol in (:I, :P, :IP)
             gradient((f, ϕ) -> logpdf(ds; f, ϕ), f, ϕ)
             MAP_joint(ds, nsteps=2, progress=false)
         end
-    end
-end
 
-Memoization.empty_all_caches!()
+        Memoization.empty_all_caches!()
+
+    end
+
+end
